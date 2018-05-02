a6727d89aa847424aa5ad7c944be073eec3b772c
Joel Nitta <joelnitta@gmail.com>
2018-05-02 09:45:19 +0900

    Create smaller set of test transcriptomes
    Make example dataset smaller so that tests run more quickly. Also verify that all 1kp data is public now, so no worries about including it in this package.
    
    
    Former-commit-id: c50b68f12bfc41c8e3e5d2ab939e52618dd5f398

--- data-raw/trim_transcriptomes.R
+++ data-raw/trim_transcriptomes.R
@@ -4,15 +4,18 @@
 library(ape)
 
 # vector of 1KP sample codes to use (all eupolypod II ferns)
-codes <- c("PSKY", "KJZG", "URCP", "AFPO", "FCHS", "UFJN", "VITX", "LHLE", "XXHP", "YOWV", "RICC", "HNDZ", "HEGQ", "OCZL", "HTFH", "MROH", "YQEC", "YJJY")
+# codes <- c("PSKY", "KJZG", "URCP", "AFPO", "FCHS", "UFJN", "VITX", "LHLE", "XXHP", "YOWV", "RICC", "HNDZ", "HEGQ", "OCZL", "HTFH", "MROH", "YQEC", "YJJY")
+# vector of 1KP sample codes to use (subset of eupolypod II ferns incl Aspleniaceae, Athyriaceae, and Woodsiaceae)
+codes <- c("PSKY", "KJZG", "YJJY", "YQEC", "AFPO", "URCP", "FCHS", "UFJN")
 
-# function to read in data, randomly subset to 10% of sequences
+# function to read in data, randomly subset to 2% of sequences
+# we want a very small dataset just for testing, not for complete analysis
 trim_fasta <- function (code) {
   seq <- read.dna(paste0("/Volumes/Storage/thelypteris/data/one_kp/", code, ".fa"), format="fasta")
-  seq <- seq[sample(1:length(seq), 0.1*length(seq))]
+  seq <- seq[sample(1:length(seq), 0.02*length(seq))]
 }
 
 trimmed_transcriptomes <- lapply(codes, trim_fasta)
 names(trimmed_transcriptomes) <- codes
 
-# usethis::use_data(trimmed_transcriptomes)
+usethis::use_data(trimmed_transcriptomes)
