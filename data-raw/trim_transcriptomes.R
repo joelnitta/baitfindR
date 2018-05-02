5974e459c2fdda2040c20316c22bbf61de7879df
Joel Nitta <joelnitta@gmail.com>
2018-05-02 14:00:58 +0900

    Download transcriptomes from 1KP site, allow downsize fraction to be set by user
    Former-commit-id: 1e888c04627382d9308853990442f10e68a7e351

--- data-raw/trim_transcriptomes.R
+++ data-raw/trim_transcriptomes.R
@@ -1,21 +1,33 @@
 # script to make small transcriptome dataset for testing
-# read in 1KP transcriptomes, randomly trim down to 10% of size
+# download 1KP transcriptomes, randomly trim down to specified fraction of original size
 
 library(ape)
 
-# vector of 1KP sample codes to use (all eupolypod II ferns)
-# codes <- c("PSKY", "KJZG", "URCP", "AFPO", "FCHS", "UFJN", "VITX", "LHLE", "XXHP", "YOWV", "RICC", "HNDZ", "HEGQ", "OCZL", "HTFH", "MROH", "YQEC", "YJJY")
-# vector of 1KP sample codes to use (subset of eupolypod II ferns incl Aspleniaceae, Athyriaceae, and Woodsiaceae)
-codes <- c("PSKY", "KJZG", "YJJY", "YQEC", "AFPO", "URCP", "FCHS", "UFJN")
+# make vector of 1KP sample codes to use
+# (all eupolypod II ferns)
+# codes <- c("PSKY", "KJZG", "URCP", "FCHS", "UFJN", "VITX", "LHLE", "XXHP", "YOWV", "RICC", "HNDZ", "HEGQ", "OCZL", "HTFH", "MROH", "YQEC", "YJJY")
+# (subset of eupolypod II ferns incl Aspleniaceae, Athyriaceae, and Woodsiaceae)
+# note that AFPO no longer is a separate sample; see "RWYZ: combined assembly of AFPO+VITX"
+codes <- c("PSKY", "KJZG", "YJJY", "YQEC", "URCP", "FCHS", "UFJN")
 
-# function to read in data, randomly subset to 2% of sequences
-# we want a very small dataset just for testing, not for complete analysis
-trim_fasta <- function (code) {
-  seq <- read.dna(paste0("/Volumes/Storage/thelypteris/data/one_kp/", code, ".fa"), format="fasta")
-  seq <- seq[sample(1:length(seq), 0.02*length(seq))]
+# function to download transcriptome from 1KP website, load the data, and randomly downsize
+# arguments
+#   sample_code: the 1KP sample code
+#   keep_frac: the percentage of transcripts to keep, e.g. 0.10 = 10%
+get_trimmed_transcriptome <- function (sample_code, keep_frac) {
+  # set a name for the to-be downloaded compressed transcriptome
+  filename <- paste0(sample_code, "-SOAPdenovo-Trans-assembly.fa.bz2")
+  # download compressed transcriptome
+  download.file(url = paste0("http://206.12.96.204/onekp/", sample_code, "-SOAPdenovo-Trans-assembly.fa.bz2"), destfile = paste0("data-raw/", filename))
+  # read in file. need bzfile() because it's compressed
+  seq <- read.dna(bzfile(paste0("data-raw/", filename)), format="fasta")
+  # randomly downsize to specified fraction of transcripts
+  seq <- seq[sample(1:length(seq), keep_frac*length(seq))]
 }
 
-trimmed_transcriptomes <- lapply(codes, trim_fasta)
+# loop across all codes, keeping 2% of transcripts
+trimmed_transcriptomes <- lapply(codes, get_trimmed_transcriptome, keep_frac = 0.02)
+# name the transcriptomes
 names(trimmed_transcriptomes) <- codes
-
-usethis::use_data(trimmed_transcriptomes)
+# export data
+usethis::use_data(trimmed_transcriptomes, overwrite = TRUE)
