d3b2773d8c653c284105404e2f2e854312490c14
Joel Nitta <joelnitta@gmail.com>
2018-05-03 15:37:11 +0900

    add documentation for proteomes
    Former-commit-id: 8d1614d6acf12b84ee44f4ce835a2be79c2813f8

--- R/data.R
+++ R/data.R
@@ -6,3 +6,19 @@
 #' All transcriptomes have been randomly subsampled to 2\% of their original size.
 #' @source \url{http://www.onekp.com/public_data.html}
 "onekp_ferns"
+
+#' Lygodium japonicum proteome
+#'
+#' Complete proteome (i.e., all coding genes) of Lygodium japonicum. Duplicate sequence IDs were identified and removed.
+#'
+#' @format A single text file in fasta format.
+#' @source \url{http://bioinf.mind.meiji.ac.jp/kanikusa/}
+"lygodium_proteome"
+
+#' Arabidopsis thaliana proteome
+#'
+#' Complete proteome (i.e., all coding genes) of Arabidopsis thaliana.
+#'
+#' @format A single text file in fasta format.
+#' @source \url{https://www.arabidopsis.org/}
+"arabidopsis_proteome"
