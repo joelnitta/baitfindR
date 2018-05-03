d1c63f2e087439800dd42086d8720c7cf4f64a3e
Joel Nitta <joelnitta@gmail.com>
2018-05-03 15:32:04 +0900

    use read_text instead of read.fasta for proteomes
    takes up much less space, and we don't need them formatted as dna objects in R anyways
    
    
    Former-commit-id: 6ba430993e424e3b69a9bae97ce93ebca91b9856

--- data-raw/get_proteomes.R
+++ data-raw/get_proteomes.R
@@ -1,7 +1,7 @@
 # download arabidopsis and lygodium proteomes
 # these will be used to make blastp database in Yang and Smith (2014) workflow
 
-library(seqinr)
+library(readr)
 
 # download arabidopsis proteome
 download.file(url = "https://www.arabidopsis.org/download_files/Sequences/TAIR10_blastsets/TAIR10_pep_20110103_representative_gene_model_updated", destfile = "data-raw/TAIR10_pep_20110103_representative_gene_model_updated.fasta")
@@ -21,11 +21,15 @@ system("tar -zxf data-raw/lygodium_predicted_potein_ver1.0RC.fasta.tar.gz -C dat
 # remove repeat names with awk
 system("awk '/^>/{f=!d[$1];d[$1]=1}f' data-raw/lygodium_predicted_potein_ver1.0RC.fasta > data-raw/lygodium_predicted_potein_ver1.0RC.clean.fasta")
 
-# read in cleaned lygodium proteome
-lygodium_proteome <- read.fasta("data-raw/lygodium_predicted_potein_ver1.0RC.clean.fasta", seqtype="AA", strip.desc=TRUE)
+# read in cleaned lygodium proteome as character vector
+# (use seqinr::read.fasta instead to store within in R as a peptide sequence)
+lygodium_proteome <- read_file("data-raw/lygodium_predicted_potein_ver1.0RC.clean.fasta")
 
-# read in arabidopsis proteome
-arabidopsis_proteome <- read.fasta("data-raw/TAIR10_pep_20110103_representative_gene_model_updated.fasta", seqtype="AA", strip.desc=TRUE)
+# note that when we want to use this, it is a plain text, so use write_file
+# write_file(lygodium_proteome_txt, "lygodium_proteome.fasta")
+
+# read in arabidopsis proteome as character vector like we did for lygodium
+arabidopsis_proteome <- read_file("data-raw/TAIR10_pep_20110103_representative_gene_model_updated.fasta")
 
 # export data
 usethis::use_data(lygodium_proteome, overwrite = TRUE, compress="xz")
