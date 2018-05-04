c999150edc94d69f120c99cad190a27947c75b5a
Joel Nitta <joelnitta@gmail.com>
2018-05-05 06:13:30 +0900

    Add taxonomic data for onekp ferns dataset
    Former-commit-id: afda60377610ed45f9dbbc2a0dd45d1cfc9794f6

--- R/data.R
+++ R/data.R
@@ -7,6 +7,21 @@
 #' @source \url{http://www.onekp.com/public_data.html}
 "onekp_ferns"
 
+#' Taxonomic data for fern transcriptomes
+#'
+#' Taxonomic data associated with the \code{\link{onekp_ferns}} dataset. Family-level taxonomy follows PPG I (2016).
+#'
+#' @format A data frame with 7 rows and 5 variables:
+#' \describe{
+#'   \item{code}{4 letter code used to identify the sample}
+#'   \item{species}{Binomial species name}
+#'   \item{genus}{Genus}
+#'   \item{specific_epithet}{Specific epithet}
+#'   \item{family}{Family}
+#' }
+#' @source \url{http://www.onekp.com/public_data.html}, Pteridophyte Phylogeny Group I. 2016. A community-derived classification for extant lycophytes and ferns. Journal of Systematics and Evolution 54:563–603.
+"onekp_data"
+
 #' Lygodium japonicum proteome
 #'
 #' Complete proteome (i.e., all coding genes) of Lygodium japonicum. Duplicate sequence IDs were identified and removed.
--- data-raw/trim_transcriptomes.R
+++ data-raw/trim_transcriptomes.R
@@ -1,11 +1,17 @@
 # script to make small transcriptome dataset for testing
 # download 1KP transcriptomes, randomly trim down to specified fraction of original size
+# also make dataframe of taxonomic data
 
+library(tidyverse)
+library(rvest)
 library(ape)
+
+# download and downsize transcriptomes ------------------------------------
 
 # make vector of 1KP sample codes to use
 # (all eupolypod II ferns)
 # codes <- c("PSKY", "KJZG", "URCP", "FCHS", "UFJN", "VITX", "LHLE", "XXHP", "YOWV", "RICC", "HNDZ", "HEGQ", "OCZL", "HTFH", "MROH", "YQEC", "YJJY")
+
 # (subset of eupolypod II ferns incl Aspleniaceae, Athyriaceae, and Woodsiaceae)
 # note that AFPO no longer is a separate sample; see "RWYZ: combined assembly of AFPO+VITX"
 codes <- c("PSKY", "KJZG", "YJJY", "YQEC", "URCP", "FCHS", "UFJN")
@@ -14,6 +20,7 @@ codes <- c("PSKY", "KJZG", "YJJY", "YQEC", "URCP", "FCHS", "UFJN")
 # arguments
 #   sample_code: the 1KP sample code
 #   keep_frac: the percentage of transcripts to keep, e.g. 0.10 = 10%
+
 get_trimmed_transcriptome <- function (sample_code, keep_frac) {
   # set a name for the to-be downloaded compressed transcriptome
   filename <- paste0(sample_code, "-SOAPdenovo-Trans-assembly.fa.bz2")
@@ -29,5 +36,46 @@ get_trimmed_transcriptome <- function (sample_code, keep_frac) {
 onekp_ferns <- lapply(codes, get_trimmed_transcriptome, keep_frac = 0.02)
 # name the transcriptomes
 names(onekp_ferns) <- codes
-# export data
+
+# prepare taxonomic data --------------------------------------------------
+
+# obtain 1KP sample data from official project website
+# read in html
+onekp_parsed <- read_html("http://www.onekp.com/samples/list.php")
+# extract tables
+tables <- html_table(onekp_parsed, fill = TRUE)
+# the table we want is the third one
+onekp_data <- tables[[3]]
+rm(onekp_parsed, tables)
+# only need code and species name
+# inspect table, see these are the first and fourth columns
+onekp_data <- onekp_data[,c(1, 4)]
+colnames(onekp_data) <- c("code", "species")
+
+# subset data to only the transcriptomes we are using
+onekp_data <- onekp_data[onekp_data$code %in% codes, ]
+
+# add genus and family-level data
+onekp_data <- onekp_data %>%
+  separate(species, c("genus", "specific_epithet"), remove=FALSE, extra="merge") %>%
+  mutate(
+    family = case_when (
+      genus == "Asplenium" ~ "Aspleniaceae",
+      genus == "Athyrium" ~ "Athyriaceae",
+      genus == "Blechnum" ~ "Blechnaceae",
+      genus == "Cystopteris" ~ "Cystopteridaceae",
+      genus == "Deparia" ~ "Athyriaceae",
+      genus == "Diplazium" ~ "Athyriaceae",
+      genus == "Gymnocarpium" ~ "Cystopteridaceae",
+      genus == "Homalosorus" ~ "Diplaziopsidaceae",
+      genus == "Onoclea" ~ "Onocleaceae",
+      genus == "Thelypteris" ~ "Thelypteridaceae",
+      genus == "Woodsia" ~ "Woodsiaceae"
+    )
+  )
+
+# export data -------------------------------------------------------------
+
 usethis::use_data(onekp_ferns, overwrite = TRUE)
+
+usethis::use_data(onekp_data, overwrite = TRUE)
