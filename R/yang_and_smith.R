fb57cf6cd0df921e5e76031372906d64271b503f
Joel Nitta <joelnitta@gmail.com>
2018-06-08 17:41:42 +0900

    Add drake tracking functionality
    Former-commit-id: 6a313b58ec3ec99c5759140cd5561a335b5ea644

--- R/yang_and_smith.R
+++ R/yang_and_smith.R
@@ -1,28 +1,64 @@
 #' fasta_to_tree
 #'
-#' Wrapper for Yang and Smith (2014) fasta_to_tree.py
+#' Wrapper for Yang and Smith (2014) \code{fasta_to_tree.py}
 #'
 #' Given a folder containing unaligned sequences in fasta format (i.e., clusters),
-#' aligns each cluster with mafft (small alignments) or pasta (large alignments),
-#' excludes poorly aligned sites with phyutility, and infers a maximum-likelihood
-#' tree with RAxML (small alignments) or fasttree (large alignments). Requires all
-#' of these programs to be installed and included in the user's `$PATH`.
+#' aligns each cluster with \code{mafft} (small clusters) or \code{pasta} (large clusters),
+#' excludes poorly aligned sites with \code{phyutility}, and infers a maximum-likelihood
+#' tree with \code{RAxML} (small clusters) or \code{fasttree} (large clusters). Requires all
+#' of these programs to be installed and included in the user's \code{$PATH}. Assumes clusters are named \code{"cluster1.fa"}, \code{"cluster2.fa"}, etc. Clusters with fewer than 1,000 sequences are considered "small," and those with more are considered "large."
 #'
-#' @param path_to_ys Character vector of length one; the path to the folder containing Y&S python scripts, e.g., "/Users/me/apps/phylogenomic_dataset_construction/"
+#' @param path_to_ys Character vector of length one; the path to the folder containing Y&S python scripts, e.g., \code{"/Users/me/apps/phylogenomic_dataset_construction/"}
+#' @param overwrite Logical; should previous output of this command be erased so new output can be written? Once erased it cannot be restored, so use with caution!
 #' @param seq_folder Character vector of length one; the name of the folder containing the fasta files.
-#' @param number_cores Numeric; number of threads to use for RAxML.
-#' @param seq_type Character vector of length one indicating type of sequences. Should either be "dna" for DNA or "aa" for proteins.
-#' @param bootstrap Logical; choose whether or not to run a bootstrap analysis for the trees.
+#' @param number_cores Numeric; number of threads to use for \code{RAxML} and \code{mafft}.
+#' @param seq_type Character vector of length one indicating type of sequences. Should either be \code{"dna"} for DNA or \code{"aa"} for proteins.
+#' @param bootstrap Logical; should run a bootstrap analysis be run for the trees?
+#' @param get_hash Logical; should the 32-byte MD5 hash be computed for all output tree files concatenated together? Used for by \code{\link{drake}} for tracking during workflows. If \code{TRUE}, this function will return the hash.
+#' @param ... Other arguments. Not used by this function, but meant to be used by \code{\link{drake}} for tracking during workflows.
+#' @return For each input cluster \code{cluster1.fa} in \code{seq_folder}, \code{cluster1.fa.mafft.aln} (small clusters) or \code{cluster1.pasta.aln} (large clusters), \code{cluster1.fa.mafft.aln-cln} (small clusters) or \code{cluster1.fa.pasta.aln-cln} (large clusters), and \code{cluster1.raxml.tre} (small clusters) or \code{cluster1.fasttree.tre} (large clusters) will be written to \code{seq_folder}. If \code{get_hash} is \code{TRUE}, the 32-byte MD5 hash be computed for all \code{.tre} files concatenated together will be returned.
 #' @author Joel H Nitta, \email{joelnitta@@gmail.com}
 #' @references Yang, Y. and S.A. Smith. 2014. Orthology inference in non-model organisms using transcriptomes and low-coverage genomes: improving accuracy and matrix occupancy for phylogenomics. Molecular Biology and Evolution 31:3081-3092. \url{https://bitbucket.org/yangya/phylogenomic_dataset_construction/overview}
 #' @examples
 #' \dontrun{fasta_to_tree(seq_folder = "some/folder/containing/fasta/seqs", number_cores = 1, seq_type = "dna", bootstrap = FALSE)}
 #' @export
-fasta_to_tree <- function (path_to_ys = pkgconfig::get_config("baitfindR::path_to_ys"), seq_folder, number_cores, seq_type, bootstrap = FALSE) {
+fasta_to_tree <- function (path_to_ys = pkgconfig::get_config("baitfindR::path_to_ys"), overwrite = FALSE, seq_folder, number_cores, seq_type, bootstrap = FALSE, get_hash = FALSE) {
+
+  # error checking
+  if(is.null(path_to_ys)) {
+    stop("Must provide 'path_to_ys' (path to Yang & Smith Phylogenomic Dataset Analysis folder)")
+  }
+
+  # modify arguments
   bootstrap <- ifelse(bootstrap, "y", "n")
   path_to_ys <- jntools::add_slash(path_to_ys)
+  seq_folder <- jntools::add_slash(seq_folder)
+
+  # optional: delete all previous output written by a previous call to fasta_to_tree.py in this folder
+  if (overwrite) {
+    files_to_delete <- list.files(seq_folder)
+    search_terms <- paste("cluster\\d*\\.fa\\.mafft\\.aln",
+                           "cluster\\d*\\.fa\\.mafft\\.aln-cln",
+                           "cluster\\d*\\.fa\\.mafft\\.aln-cln.reduced",
+                           "cluster\\d*\\.raxml\\.tre", sep = "|")
+    files_to_delete <- files_to_delete[grep(search_terms, files_to_delete)]
+    files_to_delete <- paste0(seq_folder, files_to_delete)
+    file.remove(files_to_delete)
+  }
+
+  # call fasta_to_tree.py
   arguments <- c(paste0(path_to_ys, "fasta_to_tree.py"), seq_folder, number_cores, seq_type, bootstrap)
   system2("python", arguments)
+
+  # optional: get MD5 hash of concatenated trees
+  if (get_hash) {
+    trees <- list.files(seq_folder)
+    trees <- trees[grep("\\.raxml\\.tre", trees)]
+    trees <- paste0(seq_folder, trees)
+    trees <- unlist(lapply(trees, readr::read_file))
+    hash <- digest::digest(trees)
+    return(hash)
+  }
 }
 
 #' write_fasta_files_from_mcl
@@ -38,15 +74,38 @@ fasta_to_tree <- function (path_to_ys = pkgconfig::get_config("baitfindR::path_t
 #' @param mcl_outfile Character vector of length one; the path to the output from running mcl on blast distances.
 #' @param minimal_taxa Numeric; minimal number of taxa required to be present or the clus. Default 4, the minimum number of taxa needed for an un-rooted tree.
 #' @param outdir Character vector of length one; the path to the folder where the clusters should be written.
+#' @param get_hash Logical; should the 32-byte MD5 hash be computed for all clusters concatenated together? Used for by \code{\link{drake}} for tracking during workflows. If \code{TRUE}, this function will return the hash.
+#' @param ... Other arguments. Not used by this function, but meant to be used by \code{\link{drake}} for tracking during workflows.
 #' @author Joel H Nitta, \email{joelnitta@@gmail.com}
 #' @references Yang, Y. and S.A. Smith. 2014. Orthology inference in non-model organisms using transcriptomes and low-coverage genomes: improving accuracy and matrix occupancy for phylogenomics. Molecular Biology and Evolution 31:3081-3092. \url{https://bitbucket.org/yangya/phylogenomic_dataset_construction/overview}
 #' @examples
 #' \dontrun{write_fasta_files_from_mcl(all_fasta = "some/folder/all.fasta", mcl_outfile = "some/folder/hit-frac0.4_I1.4_e5", minimal_taxa = 5, outdir = "some/folder")}
 #' @export
-write_fasta_files_from_mcl <- function (path_to_ys = pkgconfig::get_config("baitfindR::path_to_ys"), all_fasta, mcl_outfile, minimal_taxa = 4, outdir) {
+write_fasta_files_from_mcl <- function (path_to_ys = pkgconfig::get_config("baitfindR::path_to_ys"), all_fasta, mcl_outfile, minimal_taxa = 4, outdir, get_hash, ...) {
+
+  # error checking
+  if(is.null(path_to_ys)) {
+    stop("Must provide 'path_to_ys' (path to Yang & Smith Phylogenomic Dataset Analysis folder)")
+  }
+
+  # modify arguments
   path_to_ys <- jntools::add_slash(path_to_ys)
+  outdir <- jntools::add_slash(outdir)
+
+  # call write_fasta_files_from_mcl.py
   arguments <- c(paste0(path_to_ys, "write_fasta_files_from_mcl.py"), all_fasta, mcl_outfile, minimal_taxa, outdir)
   system2("python", arguments)
+
+  # optional: get MD5 hash of concatenated clusters
+  if (get_hash) {
+    clusters <- list.files(outdir)
+    clusters <- clusters[grep("\\.fa$", clusters)]
+    clusters <- paste0(out_dir, clusters)
+    clusters <- unlist(lapply(clusters, readr::read_file))
+    hash <- digest::digest(clusters)
+    return(hash)
+  }
+
 }
 
 #' blast_to_mcl
@@ -55,9 +114,10 @@ write_fasta_files_from_mcl <- function (path_to_ys = pkgconfig::get_config("bait
 #'
 #'Converts the output of an all-by-all blast query into a format that can be parsed by mcl to find clusters.
 #'
-#'@param path_to_ys Character vector of length one; the path to the folder containing Y&S python scripts, e.g., "/Users/me/apps/phylogenomic_dataset_construction/"
-#'@param blast_results Character vector of length one; the path to the tab-separated text file containing the results from an all-by-all blast search. If blast searches were run separately (i.e., one for each sample), the results should be concatenated into a single file. For the blast search, the output format should specified as: -outfmt '6 qseqid qlen sseqid slen frames pident nident length mismatch gapopen qstart qend sstart send evalue bitscore'
-#'@param hit_fraction_cutoff Numeric between 0 and 1. Indicates the minimum percentage overlap between query and target in blast results to be retained in the output. According to Y&S, "A low hit-fraction cutoff will output clusters with more incomplete sequences and much larger and sparser alignments, whereas a high hit-fraction cutoff gives tighter clusters but ignores incomplete or divergent sequences."
+#' @param path_to_ys Character vector of length one; the path to the folder containing Y&S python scripts, e.g., "/Users/me/apps/phylogenomic_dataset_construction/"
+#' @param blast_results Character vector of length one; the path to the tab-separated text file containing the results from an all-by-all blast search. If blast searches were run separately (i.e., one for each sample), the results should be concatenated into a single file. For the blast search, the output format should specified as: -outfmt '6 qseqid qlen sseqid slen frames pident nident length mismatch gapopen qstart qend sstart send evalue bitscore'
+#' @param hit_fraction_cutoff Numeric between 0 and 1. Indicates the minimum percentage overlap between query and target in blast results to be retained in the output. According to Y&S, "A low hit-fraction cutoff will output clusters with more incomplete sequences and much larger and sparser alignments, whereas a high hit-fraction cutoff gives tighter clusters but ignores incomplete or divergent sequences."
+#' @param ... Other arguments. Not used by this function, but meant to be used by \code{\link{drake}} for tracking during workflows.
 #' @author Joel H Nitta, \email{joelnitta@@gmail.com}
 #' @references Yang, Y. and S.A. Smith. 2014. Orthology inference in non-model organisms using transcriptomes and low-coverage genomes: improving accuracy and matrix occupancy for phylogenomics. Molecular Biology and Evolution 31:3081-3092. \url{https://bitbucket.org/yangya/phylogenomic_dataset_construction/overview}
 #' @examples
