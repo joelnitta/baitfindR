85c858479a560ddea482beecd8bf52c77e361dd2
Joel Nitta <joelnitta@gmail.com>
2018-06-09 17:38:14 +0900

    run devtools functions
    Former-commit-id: 998214a49b80b66a447c178b1dcc052192249193

--- R/etc.R
+++ R/etc.R
@@ -26,6 +26,6 @@ set_ys_path <- function (path) {
 #' @author Joel H Nitta, \email{joelnitta@@gmail.com}
 #' @references Yang, Y. and S.A. Smith. 2014. Orthology inference in non-model organisms using transcriptomes and low-coverage genomes: improving accuracy and matrix occupancy for phylogenomics. Molecular Biology and Evolution 31:3081-3092. \url{https://bitbucket.org/yangya/phylogenomic_dataset_construction/overview}
 #' @export
-set_ys_path <- function (path) {
+set_transdecoder_path <- function (path) {
   pkgconfig::set_config("baitfindR::set_transdecoder_path" = path)
 }
