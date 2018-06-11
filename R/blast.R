885c2ca22a982bde5c9b85d90f1d74e57d095b7a
Joel Nitta <joelnitta@gmail.com>
2018-06-11 16:55:16 +0900

    add build_blast_db
    Former-commit-id: 7f3633e1acc105ed18e266038af1b29093b73507

--- R/blast.R
+++ R/blast.R
@@ -1,10 +1,29 @@
-# Build blast db
-build_blast_db <- function (in_seqs, out_name, db_type = "nucl", other_args = "-parse_seqids", outfile) {
-  arguments <- paste0("-in ", in_seqs, " -out ", out_name, " -dbtype ", db_type, " ", other_args)
+#' build_blast_db
+#'
+#' Wrapper to call makeblastdb
+#'
+#' Takes a list of input sequences and makes them into a BLAST database. Assumes that all BLAST commands are in the user's \code{$PATH}.
+#'
+#' @param in_seqs Character vector of length one; the path to the fasta file containing the sequences to be included in the database.
+#' @param out_name Character vector of length one; name of BLAST database to be created.
+#' @param db_type Character vector of length one; "nucl" for DNA or "prot" for amino acids (proteins).
+#' @param other_args Character vector; other arguments to pass on to \code{makeblastdb}. For a list of options, run \code{makeblastdb -help}.
+#' @param ... Additional other arguments. Not used by this function, but meant to be used by \code{\link{drake}} for tracking during workflows.
+#' @return A series of files starting with \code{out_name} and ending in .phr, .pin, .pog, .psd, .psi, psq (for proteins) or .nhr, .nin, .nog, .nsd, .nsi, and .nsq (for DNA) that constitute the BLAST database in the directory specified by \code{wd}.
+#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
+#' @references \url{https://www.ncbi.nlm.nih.gov/books/NBK279690/}
+#' @export
+build_blast_db <- function (in_seqs, out_name, db_type = "nucl", other_args = NULL, ...) {
+
+  # modify arguments
+  arguments <- c(paste("-in", in_seqs), paste("-out", out_name), paste("-dbtype", db_type), other_args)
+
+  # run command
   system2("makeblastdb", arguments)
+
 }
 
-#' blastp_long_orfs
+#' blastp
 #'
 #' Wrapper to call blastp
 #'
