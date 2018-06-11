00ba2b63293617e831085931b144d70f1889493b
Joel Nitta <joelnitta@gmail.com>
2018-06-11 15:52:16 +0900

    use processx so we can specify wd
    Former-commit-id: bfc20f5a064b0d15dc72a3e8c93884240b015657

--- R/transdecoder.R
+++ R/transdecoder.R
@@ -1,17 +1,33 @@
-# Wrapper to call transdecoder for a particular sequence
-# This produces one .transdecoder_dir folder as output per transcriptome in the working directory
-transdecoder_long_orfs <- function (transcriptome, transdecoder_result_folder, path_to_transdecoder = "~/apps/TransDecoder/", other_args = NULL, outfile) {
+#' transdecoder_long_orfs
+#'
+#' Wrapper to call TransDecoder.LongOrfs
+#'
+#' Extracts long open reading frames (ORFs) from a fasta file containing transcript sequences (i.e., the transcriptome).
+#'
+#' @param path_to_transdecoder Character vector of length one; the path to the folder containing TransDecoder scripts, e.g., \code{"/Users/me/apps/TransDecoder/"}
+#' @param transcriptome Character vector of length one; the path to the fasta file containing transcript sequences (i.e., the transcriptome).
+#' @param wd Character vector of length one; the directory where the command will be run, and the output folder created.
+#' @param other_args Character vector; other arguments to pass to TransDecoder. Each should be an element of the vector.
+#' @param ... Additional other arguments. Not used by this function, but meant to be used by \code{\link{drake}} for tracking during workflows.
+#' @return An output folder in the working directory containing base frequencies and .cds, .gff3, and .pep files for the recovered ORFs. The folder will be named \code{<transcriptome>.transdecoder_dir}, where \code{<transcriptome>} is the value of that argument.
+#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
+#' @references \url{http://transdecoder.github.io}
+#' @examples
+#' \dontrun{transdecoder_long_orfs("some/transcriptome_file.fa")}
+transdecoder_long_orfs <- function (path_to_transdecoder = pkgconfig::get_config("baitfindR::path_to_transdecoder"), transcriptome, wd, other_args = NULL, ...) {
 
-  arguments <- paste0("-t ", transcriptome, " ", other_args)
+  # error checking
+  if(is.null(path_to_transdecoder)) {
+    stop("Must provide 'path_to_transdecoder' (path to TransDecoder folder)")
+  }
 
-  # Normally I would use system2() but for some reason it doesn't like the path in the command.
-  # system() seems to have no issues with this
-  system(paste0(path_to_transdecoder, "TransDecoder.LongOrfs ", arguments))
+  # modify arguments
+  path_to_transdecoder <- jntools::add_slash(path_to_transdecoder)
+  arguments <- c("-t", transcriptome, other_args)
 
-  # Copy the output (transdecoder_result_folder) to translation subfolder,
-  # using rsync to overwrite any existing files
-  system2("rsync", c("-a", here::here(transdecoder_result_folder), here::here("translation")))
+  # modify command
+  command <- paste0(path_to_transdecoder, "TransDecoder.LongOrfs")
 
-  # Delete the copy remaining in the working folder
-  system2("rm", c("-r ", here::here(transdecoder_result_folder)))
+  # run command
+  processx::run(command, arguments, wd = wd)
 }
