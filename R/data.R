32a0088728a660cbdde6304429ba379cd95a2837
Joel Nitta <joelnitta@gmail.com>
2018-06-15 16:57:36 +0900

    Update data documentation
    Former-commit-id: 6bb1e4ffa546120a40f430d78d8ac7d7087e2dcf

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Taxonomic data for fern transcriptomes
 #'
-#' Taxonomic data associated with the \code{\link{onekp_ferns}} dataset. Family-level taxonomy follows PPG I (2016).
+#' Taxonomic data for seven fern transcripomes downloaded from the onekp project. Family-level taxonomy follows PPG I (2016).
 #'
 #' @format A data frame with 7 rows and 5 variables:
 #' \describe{
@@ -10,5 +10,7 @@
 #'   \item{specific_epithet}{Specific epithet}
 #'   \item{family}{Family}
 #' }
-#' @source \url{http://www.onekp.com/public_data.html}, Pteridophyte Phylogeny Group I. 2016. A community-derived classification for extant lycophytes and ferns. Journal of Systematics and Evolution 54:563–603.
+#' @source \url{http://www.onekp.com/public_data.html}
+#'
+#' Pteridophyte Phylogeny Group I. 2016. A community-derived classification for extant lycophytes and ferns. Journal of Systematics and Evolution 54:563–603.
 "onekp_data"
