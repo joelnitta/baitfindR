eb7ed24cb3f28df9c02f1d7105f514e77add8c87
Joel Nitta <joelnitta@gmail.com>
2018-07-18 14:46:49 +0900

    Delete set_transdecoder_path function

--- R/etc.R
+++ R/etc.R
@@ -14,22 +14,6 @@ set_ys_path <- function (path) {
   pkgconfig::set_config("baitfindR::path_to_ys" = path)
 }
 
-#' set_transdecoder_path
-#'
-#' Set the default path to the folder containing \code{transdecoder} scripts.
-#'
-#' This will overwrite the default value of the \code{path_to_transdecoder} argument for all \code{baitfindR} functions.
-#'
-#' @param path Character vector of length one; the complete path to the folder containing \code{transdecoder}, e.g., \code{"/Users/me/apps/transdecoder/"}
-#' @examples
-#' set_ys_path("/Users/me/apps/phylogenomic_dataset_construction/")
-#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
-#' @references Yang, Y. and S.A. Smith. 2014. Orthology inference in non-model organisms using transcriptomes and low-coverage genomes: improving accuracy and matrix occupancy for phylogenomics. Molecular Biology and Evolution 31:3081-3092. \url{https://bitbucket.org/yangya/phylogenomic_dataset_construction/overview}
-#' @export
-set_transdecoder_path <- function (path) {
-  pkgconfig::set_config("baitfindR::path_to_transdecoder" = path)
-}
-
 #' make_dir
 #'
 #' Make a directory
