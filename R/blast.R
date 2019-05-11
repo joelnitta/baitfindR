cf13e16518da95810cdaede6d7ea2d1ce7bd296d
Joel Nitta <joelnitta@gmail.com>
2019-05-11 17:20:19 -0400

    Use processx::run() for build_blast_db()
    Add example

--- R/blast.R
+++ R/blast.R
@@ -6,12 +6,18 @@
 #'
 #' @param in_seqs Character vector of length one; the path to the fasta
 #' file containing the sequences to be included in the database.
-#' @param out_name Character vector of length one; name of BLAST database
-#' to be created.
 #' @param db_type Character vector of length one; "nucl" for DNA or "prot"
 #' for amino acids (proteins).
-#' @param other_args Character vector; other arguments to pass on to
-#' \code{makeblastdb}. For a list of options, run \code{makeblastdb -help}.
+#' @param out_name Character vector of length one; name of BLAST database
+#' to be created (optional). This will be used to name all database files;
+#' if omitted, the name of the `in_seqs` file will be used instead.
+#' @param title Character vector of length one; title of BLAST database
+#' to be created (optional).
+#' @param parse_seqids Logical; should the makeblastdb flag
+#' "parse_seqids" be used?
+#' @param wd Character vector of length one; working directory. The blast
+#' database will be made here.
+#' @param echo Logical; should standard error and output be printed?
 #' @param ... Additional other arguments. Not used by this function, but
 #' meant to be used by \code{\link[drake]{drake_plan}} for tracking
 #' during workflows.
@@ -21,18 +27,59 @@
 #' the working directory.
 #' @author Joel H Nitta, \email{joelnitta@@gmail.com}
 #' @references \url{https://www.ncbi.nlm.nih.gov/books/NBK279690/}
+#' @examples
+#' \dontrun{
+#' library(ape)
+#' data(woodmouse)
+#' temp_dir <- tempdir()
+#' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse.fasta"))
+#' list.files(temp_dir)
+#' build_blast_db(
+#'   fs::path(temp_dir, "woodmouse.fasta"),
+#'   title = "test db",
+#'   out_name = "wood",
+#'   parse_seqids = TRUE,
+#'   wd = temp_dir)
+#' list.files(temp_dir)
+#' fs::file_delete(list.files(temp_dir, full.names = TRUE))
+#' }
 #' @export
-build_blast_db <- function (in_seqs, out_name, db_type = "nucl",
-                            other_args = NULL, ...) {
+build_blast_db <- function (in_seqs, db_type = "nucl",
+                            out_name = NULL,
+                            title = NULL,
+                            parse_seqids = FALSE,
+                            echo = TRUE,
+                            wd, ...) {
 
-  # modify arguments
-  arguments <- c(paste("-in", in_seqs),
-                 paste("-out", out_name),
-                 paste("-dbtype", db_type),
-                 other_args)
+  # Check input
+  assertthat::assert_that(assertthat::is.string(in_seqs))
+  assertthat::assert_that(assertthat::is.string(db_type))
+  assertthat::assert_that(assertthat::is.string(wd))
+  assertthat::assert_that(is.logical(parse_seqids))
+  assertthat::assert_that(assertthat::is.string(title) | is.null(title))
+  assertthat::assert_that(assertthat::is.string(out_name) | is.null(out_name))
+
+  assertthat::assert_that(db_type %in% c("nucl", "prot"))
+
+  wd <- fs::path_abs(wd)
+  assertthat::assert_that(assertthat::is.dir(wd))
+
+  in_seqs <- fs::path_abs(in_seqs)
+  assertthat::assert_that(assertthat::is.readable(in_seqs))
+
+  # Prepare arguments
+  parse_seqids <- if(isTRUE(parse_seqids)) "-parse_seqids" else NULL
+  title <- if(!is.null(title)) c("-title", title) else NULL
+  out_name <- if(!is.null(out_name)) c("-out", out_name) else NULL
+
+  arguments <- c("-in", in_seqs,
+                 "-dbtype", db_type,
+                 parse_seqids,
+                 out_name,
+                 title)
 
   # run command
-  system2("makeblastdb", arguments)
+  processx::run("makeblastdb", arguments, wd = wd, echo = echo)
 
 }
 
