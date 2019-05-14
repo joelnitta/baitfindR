1edd3c6e7d920ba10b54b0a03e5a8a69f39a4c00
Joel Nitta <joelnitta@gmail.com>
2019-05-14 18:56:28 +0000

    Add blast_n_list function

--- R/blast.R
+++ R/blast.R
@@ -216,7 +216,7 @@ blast_p <- function (query,
 #' data(woodmouse)
 #' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse.fasta"))
 #'
-#' # Make protein blast database
+#' # Make blast database
 #' build_blast_db(
 #'   fs::path(temp_dir, "woodmouse.fasta"),
 #'   db_type = "nucl",
@@ -278,5 +278,132 @@ blast_n <- function (query,
 
   # run command
   processx::run("blastn", arguments, wd = wd, echo = echo)
+
+}
+
+#' Run blastn on all fasta files in a folder.
+#'
+#' Output is written to the same folder containing the input files.
+#'
+#' @param fasta_folder Path to the folder containing fasta files to BLAST.
+#' @param fasta_pattern Optional; pattern used for matching with grep. Only
+#' files with names matching the pattern will be included in the
+#' BLAST search.
+#' @param database_path Path to the BLAST database, including the database
+#' name.
+#' @param out_ext File extension used for BLAST results files. The result of
+#' each BLAST search will be a file with the same name as the input fasta files,
+#' but with this extension appended.
+#' @param outfmt String; format to use for BLAST output. See
+#' https://www.ncbi.nlm.nih.gov/books/NBK279684/ (Table C1) for details.
+#' @param other_args Character vector; other arguments to pass on to
+#' \code{blastn}. For a list of options, run \code{blastn -help}.
+#' @param overwrite Logical: should old output be erased before running this
+#' function? "Old output" will be determined by matching any file names with
+#' `out_ext`.
+#' @param echo Logical; should standard error and output be printed?
+#' @param get_hash Logical; if TRUE, the MD5 hash of the output will be
+#' returned.
+#' @param ... Additional other arguments. Not used by this function,
+#' but meant to be used by \code{\link[drake]{drake_plan}} for tracking
+#' during workflows.
+#' @return NULL or character vector if `get_hash` is TRUE.
+#' Externally, a text file file with the results of the blastn
+#' search, named by adding `out_ext` to each input fasta file name.
+#' @author Joel H Nitta, \email{joelnitta@@gmail.com}
+#' @references \url{https://www.ncbi.nlm.nih.gov/books/NBK279690/}
+#'
+#' @examples
+#' library(ape)
+#'
+#' # Make temp dir for storing files
+#' temp_dir <- fs::dir_create(fs::path(tempdir(), "baitfindR_example"))
+#'
+#' # Write out ape::woodmouse dataset as DNA
+#' data(woodmouse)
+#' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse.fasta"))
+#' ape::write.FASTA(woodmouse, fs::path(temp_dir, "woodmouse2.fasta"))
+#'
+#' # Make blast database
+#' build_blast_db(
+#'   fs::path(temp_dir, "woodmouse.fasta"),
+#'   db_type = "nucl",
+#'   out_name = "wood",
+#'   parse_seqids = TRUE,
+#'   wd = temp_dir)
+#'
+#' # Blast the original sequences against the database
+#' blast_n_list(
+#'   fasta_folder = temp_dir,
+#'   fasta_pattern = "fasta",
+#'   database_path = fs::path(temp_dir, "wood")
+#' )
+#'
+#' # Take a look at the results.
+#' readr::read_tsv(
+#'   fs::path(temp_dir, "woodmouse.tsv"),
+#'   col_names = FALSE
+#'   )
+#'
+#' readr::read_tsv(
+#'   fs::path(temp_dir, "woodmouse2.tsv"),
+#'   col_names = FALSE
+#'   )
+#'
+#' # Cleanup.
+#' fs::file_delete(temp_dir)
+#' @export
+blast_n_list <- function (fasta_folder,
+                          fasta_pattern,
+                          database_path,
+                          out_ext = "tsv",
+                          outfmt = "6",
+                          other_args = NULL,
+                          overwrite = FALSE,
+                          echo = FALSE,
+                          get_hash = TRUE, ...) {
+
+  # Check input
+  assertthat::assert_that(assertthat::is.dir(fasta_folder))
+  assertthat::assert_that(assertthat::is.string(fasta_pattern))
+  assertthat::assert_that(assertthat::is.string(database_path))
+  assertthat::assert_that(assertthat::is.string(out_ext))
+  assertthat::assert_that(assertthat::is.string(outfmt))
+  assertthat::assert_that(is.character(other_args) | is.null(other_args))
+  assertthat::assert_that(is.logical(overwrite))
+  assertthat::assert_that(is.logical(echo))
+  assertthat::assert_that(is.logical(get_hash))
+
+  # Setup arguments
+  fasta_folder <- fs::path_abs(fasta_folder)
+  database_path <- fs::path_abs(database_path)
+  search_terms <- glue::glue("\\.{out_ext}$")
+
+  # optional: delete all previous output written in this folder based on output extension
+  if (isTRUE(overwrite)) delete_old_output(fasta_folder, search_terms)
+
+  # List all fasta files in folder
+  fasta_files <- list.files(fasta_folder, pattern = fasta_pattern, full.names = TRUE)
+
+  # Make list of result files to write
+  # (Just append out_ext to each input fasta file)
+  results_list <- purrr::map_chr(fasta_files, ~fs::path_ext_set(., ext = out_ext))
+
+  # Loop blast search over lists
+  purrr::walk2(
+    fasta_files,
+    results_list,
+    ~ blast_n(
+      query = .x,
+      out_file = .y,
+      wd = fasta_folder,
+      database = database_path,
+      outfmt = outfmt,
+      other_args = other_args,
+      echo = echo)
+  )
+
+  # Optional: get MD5 hash of output
+  if (isTRUE(get_hash)) get_out_hash(fasta_folder, search_terms)
 
 }
